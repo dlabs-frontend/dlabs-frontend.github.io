f81418d16c6e138a281a34a6a8f20fef6873937a
jipirfandyddi <jip.irfandy@dayadimensi.co.id>
2022-03-24 14:10:22 +0700

    chore: update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,9 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dlabs-frontend',
+  projectName: 'DLabs Frontend',
+  trailingSlash: false,
 
   presets: [
     [
