8e0e68a121173ac6fea1b7aee540c51d275ae29c
jipirfandyddi <jip.irfandy@dayadimensi.co.id>
2022-03-24 14:35:46 +0700

    chore: update `projectName` so it doens't have space

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'dlabs-frontend',
-  projectName: 'DLabs Frontend',
+  projectName: 'dlabs-frontend',
   trailingSlash: false,
 
   presets: [
